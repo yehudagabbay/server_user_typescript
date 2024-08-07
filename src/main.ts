234c136155a5084b7286b29adea4a0298fa396f7
Zen-ya <elya64@gmail.com>
2024-08-07 23:54:05 +0300

    h

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import path from 'path';
 dotenv.config();
 
 // Définir le port
-const PORT = process.env.PORT || 3000; // Ajout d'un port par défaut si process.env.PORT n'est pas défini
+const PORT = process.env.PORT!; // Ajout d'un port par défaut si process.env.PORT n'est pas défini
 
 // Créer l'application Express
 const app: Express = express();
@@ -33,7 +33,7 @@ app.use(
 
 // Configurer CORS
 const corsOptions: cors.CorsOptions = {
-  origin: 'http://localhost:5173', // Permet uniquement cette origine
+  origin: 'https://server-user-typescript-2.onrender.com/', // Permet uniquement cette origine
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permet ces méthodes HTTP
   allowedHeaders: ['Content-Type', 'Authorization'], // Permet ces en-têtes
   credentials: true, // Permet l'envoi des cookies et des informations d'authentification
@@ -48,15 +48,6 @@ app.set('views', path.join(__dirname, 'views/'));
 
 // Routes utilisateur
 app.use('/api/users', userRouter);
-
-// Gestion des erreurs CORS (si nécessaire, mais déjà couvert par `cors`)
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 // Démarrer le serveur
 app.listen(PORT, () => {
